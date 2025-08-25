d1a267b7898858d00b23f83e932304f4818b17a4
不插电 <r27153733@gmail.com>
2025-08-26 00:37:10 +0800

    feat: tls config new https

--- natnet/nathttp/http.go
+++ natnet/nathttp/http.go
@@ -2,6 +2,7 @@ package nathttp
 
 import (
 	"context"
+	"crypto/tls"
 	"errors"
 	"github.com/r27153733/natlisten/natnet"
 	"net"
@@ -71,6 +72,42 @@ func ReuseListenAndServeTLSIPV4PubNat(s *http.Server, certFile, keyFile string, 
 	}
 
 	return s.ServeTLS(ln, certFile, keyFile)
+}
+
+func ReuseListenAndServeTLSConfigIPV4PubNat(s *http.Server, config *tls.Config, fn func(ip net.IP, port int) error) error {
+	addr := s.Addr
+	if addr == "" {
+		addr = ":https"
+	}
+
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	defer ln.Close()
+
+	lnConn, ok := ln.(syscall.Conn)
+	if !ok {
+		return errors.New("could not convert to syscall.Conn")
+	}
+	err = natnet.SetListenerReuse(lnConn)
+	if err != nil {
+		return err
+	}
+
+	localAddr, ok := ln.Addr().(*net.TCPAddr)
+	if !ok {
+		return errors.New("could not convert to net.TCPAddr")
+	}
+	err = natnet.IPV4PubNat(context.Background(), localAddr, fn)
+	if err != nil {
+		return err
+	}
+
+	httpsLn := tls.NewListener(ln, config)
+
+	return s.Serve(httpsLn)
 }
 
 func HttpReuseListenAndServe(s *http.Server) error {
